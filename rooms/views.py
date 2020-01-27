6c61eb67f230f8ec210a6b52844787c2a811225c
Geonju Park <pkju98@naver.com>
2020-01-27 22:54:58 +0900

    Handmade Fucking Form

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
-from django.views.generic import ListView
-from django.http import Http404
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from django_countries import countries
 from . import models
 
 
@@ -16,9 +15,105 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+# CBV
+class RoomDetail(DetailView):
+    model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(request.GET.get("room_type", 0))  #  현재 룸 타입
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
+    s_amenities = request.GET.getlist("amenities")
+    s_facilities = request.GET.getlist("facilities")
+    s_rules = request.GET.getlist("house_rules")
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+        "s_amenities": s_amenities,
+        "s_facilities": s_facilities,
+        "s_rules": s_rules,
+        "price": price,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "beds": beds,
+        "baths": baths,
+        "instant": instant,
+        "superhost": superhost,
+    }
+
+    room_types = models.RoomType.objects.all()  #  모든 룸 타입
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    house_rules = models.HouseRule.objects.all()
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+        "house_rules": house_rules,
+    }
+
+    filter_args = {}  #  필터링 인자 dictionary
+
+    #  필터링 조건 추가
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk__exact"] = room_type
+
+    if price > 0:
+        filter_args["price__lte"] = price
+
+    if guests > 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms > 0:
+        filter_args["beds__gte"] = beds
+
+    if baths > 0:
+        filter_args["bath__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            rooms = rooms.filter(amenities__pk=int(s_amenity))
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            rooms = rooms.filter(facilities__pk=int(s_facility))
+
+    #  필터링 조건 추가 끝
+    #  필터링 결과를 rooms에 담아서 context로 보냄
+
+    return render(request, "rooms/search.html", {**form, **choices, "rooms": rooms},)
+
+
+# FBV
+# def room_detail(request, pk):
+#     try:
+#         room = models.Room.objects.get(pk=pk)
+#         return render(request, "rooms/detail.html", {"room": room})
+#     except models.Room.DoesNotExist:
+#         raise Http404()
