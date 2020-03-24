b32b79da411ebd54bb02fda41bbbb7ac7688c738
Geonju Park <pkju98@naver.com>
2020-03-24 18:53:12 +0900

    AWS 2

--- config/rooms/templatetags/sexy_capitals.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def sexy_capitals(value):
-    print(value)
-    return value.capitalize()
--- config/users/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-# Configuration File
-class UsersConfig(AppConfig):
-    name = "users"
